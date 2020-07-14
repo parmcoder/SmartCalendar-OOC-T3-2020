2849d87e018702117ad7535ab8de97f5d27e86a0
Supakrit <supakrit.nuc@student.mahidol.edu>
2020-07-14 20:56:06 +0700

    AdminService

--- src/main/java/ooc/squishtable/service/IAdminService.java
+++ src/main/java/ooc/squishtable/service/IAdminService.java
@@ -1,7 +1,21 @@
 package ooc.squishtable.service;
 
+import ooc.squishtable.model.AppUser;
+
+import java.util.List;
+
 public interface IAdminService {
-    void removeUser();
-    void addUser();
-    void updateUser();
+    List<AppUser> findAll();
+
+    Boolean removeUser(AppUser user);
+
+    Boolean checkExistedUser(AppUser user);
+
+    Boolean addNewUser(AppUser user);
+
+    Boolean addNewAdmin(AppUser user);
+
+    Boolean updateUserInfo(String userName, AppUser user);
+
+    AppUser getCurrentInfo(String username);
 }
