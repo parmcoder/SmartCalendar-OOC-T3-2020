135253832d1b356d72b218578a9a63f369c45fbd
Supakrit <supakrit.nuc@student.mahidol.edu>
2020-07-14 20:38:15 +0700

    Finish AppUserDao

--- src/main/java/ooc/squishtable/dao/AppUserDao.java
+++ src/main/java/ooc/squishtable/dao/AppUserDao.java
@@ -3,10 +3,13 @@ package ooc.squishtable.dao;
 import ooc.squishtable.mapper.UserMapper;
 import ooc.squishtable.model.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class AppUserDao extends JdbcDaoSupport {
@@ -16,7 +19,96 @@ public class AppUserDao extends JdbcDaoSupport {
     @Autowired
     public DataSource dataSource;
 
+    @PostConstruct
+    public void init(){
+        setDataSource(dataSource);
+    }
+
     public AppUser findUserAccount(String userName) {
-        return new AppUser();
+        String sql = UserMapper.BASE_SQL + " where u.User_Name = ? ";
+
+        Object[] params = new Object[] { userName };
+        try {
+            AppUser appUserInfo = getJdbcTemplate().queryForObject(sql, params, mapper);
+            return appUserInfo;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<AppUser> getAllUsers() {
+        String sql = UserMapper.BASE_SQL;
+        List<AppUser> resultList;
+        try{
+            resultList = getJdbcTemplate().query(sql, mapper);
+
+        }catch(EmptyResultDataAccessException e){
+            System.out.println("Null!");
+            resultList = null;
+        }
+        return resultList;
+    }
+
+    public void insertUser(AppUser user, int authority){
+        /*
+        ? find total count and then insert to every table
+         */
+        String sqlForCount = "select max(USER_ID) from APP_USER";
+        String sqlForCount2 = "select max(ID) from USER_ROLE";
+        String sqlForInsert1 = "insert into APP_USER (USER_ID, USER_NAME, ENCRYPTED_PASSWORD, ENABLED)\n" +
+                "values (?\n" +
+                "        , ?\n" +
+                "        , ?\n" +
+                "        , 1)";
+        String sqlForInsert2 = "insert into USER_ROLE (ID, USER_ID, ROLE_ID)\n" +
+                "values (?, ?, ?)";
+        try{
+            Long lastId = getJdbcTemplate().queryForObject(sqlForCount, Long.class)+1;
+            Long lastRole = getJdbcTemplate().queryForObject(sqlForCount2, Long.class)+1;
+            Object[] params = new Object[]{ lastId, user.getUsername(), user.getPassword() };
+            getJdbcTemplate().update(sqlForInsert1, params);
+
+            if(authority>0){
+                params = new Object[]{ lastRole, lastId, 1};
+                getJdbcTemplate().update(sqlForInsert2, params);
+                lastRole++;
+            }
+            params = new Object[]{ lastRole, lastId, 2};
+            getJdbcTemplate().update(sqlForInsert2, params);
+        }catch(EmptyResultDataAccessException e){
+            System.out.println("Null!");
+        }
+    }
+
+    public void removeUser(AppUser user){
+        /*
+        ? Remove user from both tables
+         */
+
+        String sqlForRemove1 = "delete from APP_USER where USER_ID = ?;";
+        String sqlForRemove2 = "delete from USER_ROLE where USER_ID = ?;";
+        try{
+            Object[] params = new Object[]{ user.getId() };
+            getJdbcTemplate().update(sqlForRemove2, params);
+            getJdbcTemplate().update(sqlForRemove1, params);
+        }catch(EmptyResultDataAccessException e){
+            System.out.println("Null!");
+        }
+    }
+
+    public void updateUser(AppUser user){
+        /*
+        ? Update
+         */
+
+        String sqlForUpdate = "update APP_USER set USER_NAME = ?, USER_STATUS = ? where USER_ID = ?";
+//        String sqlForRemove2 = "delete from USER_ROLE where USER_ID = ?;";
+        try{
+            Object[] params = new Object[]{ user.getUsername(), user.getStatus()  ,user.getId() };
+            getJdbcTemplate().update(sqlForUpdate, params);
+//            getJdbcTemplate().update(sqlForRemove1, params);
+        }catch(EmptyResultDataAccessException e){
+            System.out.println("Null!");
+        }
     }
 }
