fb4becd7abce0130d2a88a869376dc3259754c65
parmcoder <possawat2017@hotmail.com>
2020-07-18 00:33:26 +0700

    made some rest requests

--- backend/src/main/java/ooc/squishtable/controller/BackEndController.java
+++ backend/src/main/java/ooc/squishtable/controller/BackEndController.java
@@ -8,20 +8,19 @@ import ooc.squishtable.service.IUserService;
 import ooc.squishtable.service.UserDetailsServiceImpl;
 import ooc.squishtable.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-@Controller
-public class MainController {
+@RestController
+@RequestMapping("api/")
+public class BackEndController {
 
     /*
      Just good services
@@ -37,6 +36,40 @@ public class MainController {
 
     private Boolean success = true;
     private ErrorType error = ErrorType.NO_ERROR;
+
+    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+    public static final String SECURED_TEXT = "Hello from the secured resource!";
+
+
+    @RequestMapping(path = "/hello")
+    public String sayHello() {
+        return HELLO_TEXT;
+    }
+
+    @RequestMapping(path = "/user/{lastName}/{firstName}", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public long addNewUser (@PathVariable("lastName") String lastName, @PathVariable("firstName") String firstName) {
+
+        return 5;
+    }
+
+    @GetMapping(path = "/user/{id}")
+    public AppUser getUserById(@PathVariable("id") long id) {
+
+        return new AppUser();
+    }
+
+    @RequestMapping(path="/secured", method = RequestMethod.GET)
+    public @ResponseBody String getSecured() {
+        return SECURED_TEXT;
+    }
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectApi() {
+        return "forward:/";
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showFirstPage(Model model, Principal principal) {
