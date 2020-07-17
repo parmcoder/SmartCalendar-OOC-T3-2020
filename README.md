666ea3c77fdc2c342b59f946c305296381acf520
parmcoder <possawat2017@hotmail.com>
2020-07-18 01:27:37 +0700

    Setting up new environment to work together

--- README.md
+++
@@ -1,4 +0,0 @@
-# SmartCalendar-OOC-T3-2020
-Free trello
-
-Codes are avaiable in src file.
--- backend/src/main/java/ooc/squishtable/dao/IRoleDao.java
+++
@@ -1,7 +0,0 @@
-package ooc.squishtable.dao;
-
-import java.util.List;
-
-public interface IRoleDao {
-    List<String> getRoleNames(Long userId);
-}
