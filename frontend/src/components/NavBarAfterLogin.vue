f8ad03961879d4800f0b28a64ba3becd786a5483
parmcoder <possawat2017@hotmail.com>
2020-07-22 22:48:04 +0700

    made navbar for index

---
+++ frontend/src/components/NavBarAfterLogin.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <v-toolbar flat app color="orange accent-3" dense dark>
+            <v-btn @click="drawer = !drawer" class="grey--text"> <v-icon> home </v-icon></v-btn>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer" class="orange">
+            <p>test</p>
+        </v-navigation-drawer>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                drawer: false
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
