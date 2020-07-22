5f56527e57e2872b686b04ecc95fb4e01c5fb632
parmcoder <possawat2017@hotmail.com>
2020-07-23 02:09:03 +0700

    fixed logout

--- backend/src/main/java/ooc/squishtable/main/jwt/AuthTokenFilter.java
+++ backend/src/main/java/ooc/squishtable/main/jwt/AuthTokenFilter.java
@@ -50,7 +50,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-        logger.info(headerAuth);
+//        logger.info(headerAuth);
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7, headerAuth.length());
         }
--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -263,13 +263,11 @@
                 return Math.floor((b - a + 1) * Math.random()) + a
             },
             handleLogout() {
-                if (this.user.username && this.user.password) {
-                    this.$store.dispatch('auth/logout', this.user).then(
+                this.$store.dispatch('auth/logout', this.user).then(
                         () => {
                             this.$router.push('/');
                         }
                     );
-                }
             }
         },
     }
