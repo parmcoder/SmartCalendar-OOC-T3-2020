92e7d8256a1a2017ee821957bfe77d6be71f8e2a
parmcoder <possawat2017@hotmail.com>
2020-07-23 07:17:10 +0700

    change to squishtable

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/user/';
+const API_URL = 'http://squishtable.com/api/user/';
 
 class UserService {
     /**
